9ac25773875e172405b1af825f8824ba8b8201fb
Vikas Jha <jhavikas2004@gmail.com>
2026-07-12 20:13:52 +0530

    setup monitoring with prometheus and grafana

--- order-service/src/main/java/com/vikas/order_service/model/CreateOrderDTO.java
+++ order-service/src/main/java/com/vikas/order_service/model/CreateOrderDTO.java
@@ -1,10 +1,10 @@
 package com.vikas.order_service.model;
 
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 
 public record CreateOrderDTO(
-        @NotNull(message = "productId cannot be empty...") Long productId,
-        @NotNull(message = "quantity cannot be empty...")
-                @Min(value = 1, message = "ordered quantity must be at least 1...")
-                Integer quantity) {}
+                @NotNull(message = "productId cannot be empty...") @Min(value = 1, message = "productId should be more than 0") @Max(value = 3, message = "productId should be less than 4") Long productId,
+                @NotNull(message = "quantity cannot be empty...") @Min(value = 1, message = "ordered quantity must be at least 1...") Integer quantity) {
+}
